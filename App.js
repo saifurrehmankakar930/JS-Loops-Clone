f353652667b7fc31064c7e0a9661945bcd0ef5fd
saifurrehmankakar930 <saifurrehamkaka930@gmail.com>
2025-07-01 08:50:18 -0700

    initial

---
+++ App.js
@@ -0,0 +1,159 @@
+// console.log("hello world");
+
+
+// let username = "Sir Abdullah";
+// let age = 20;
+// let email = "mabdullah@gmail.com";
+
+// // console.log("username is " + username + " and age is " + age + " and email is " + email + " " + age);
+// console.log(`hello world ${username} and age is ${age} and email is ${email}`);
+
+
+
+// for state
+
+
+// i = iteration
+
+// initialization condition increment
+
+// infinite loop
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// let num = 10;
+
+
+// // 10 * 1 = 10
+// // 10 * 2 = 20      
+
+
+
+
+
+
+
+
+// let num = 48;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${num} * ${i} = ${num * i}`);
+// }
+
+
+
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+    
+// }
+
+
+
+
+// nested loop
+
+// for(let i = 0; i < 5; i++){
+//     console.log("parent loop ==>" , i);
+//     for(let j = 0; j < 5; j++){
+//         console.log("child loop ==>" , j);
+//     }
+// }
+
+
+
+// for(let i = 1; i <= 1000; i++){
+//     console.log(`Table of ${i}`);
+//     for(let j = 1; j <= 10; j++){
+//         console.log(`${i} * ${j} = ${i * j}`);
+//     }
+//     console.log('--------------------');
+    
+    
+// }
+
+
+
+// const input = document.querySelector("#numberInput");
+// const result = document.querySelector("#result");
+
+
+// function generateTable(){
+//     const number = +input.value;
+//     for(let i = 1; i <= 10; i++){
+//         console.log(`${number} * ${i} = ${number * i}`);
+//         result.innerHTML += `<p>${number} * ${i} = ${number * i}</p>`; 
+        
+//     }
+    
+// }
+
+
+
+
+// dom manipulation
+// nested loop
+// loops with arrays
+// while loop
+// do while loop
+
+
+
+// const fruits = ["apple", "banana", "orange", "mango", "grape"];
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i] , i);
+    
+// }
+
+// const result = document.querySelector("#result");
+
+// const cities = ["Karachi", "Lahore", "Quetta", "Islamabad", "Peshawar"];
+
+// for(let i = 0; i < cities.length; i++){
+//     console.log(cities[i]);
+//     result.innerHTML += `<p>${cities[i]}</p>`;
+// }
+
+
+// break & continue
+
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+//     if(i == 5){
+//         console.log("loop is breaked at 5");
+//         break 
+//     }
+// }
+
+
+// for(let i = 0; i < 10; i++){
+//     if(i === 5){
+//         // console.log("loop is breaked at 5");
+//         continue 
+//     }
+//     console.log(i);
+   
+// }
+
+
+
+
+
+
+// let i = 11;
+// while(i < 10){
+//     console.log(i);
+//     i++    
+// }
+
+
+
+// let i = 11
+// do {
+//     console.log(i);
+//     i++
+    
+// } while (i < 10);
